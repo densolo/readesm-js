5d55f16378c04bfee4db9112033b7b23cb9ed9e3
jcardus <asklocation.net@gmail.com>
2022-09-08 14:41:41 +0100

    Update blockFactory.ts
    bug checking vu / card

--- src/VuBlocks/blockFactory.ts
+++ src/VuBlocks/blockFactory.ts
@@ -2,10 +2,10 @@
 import TopLevelBlock from 'DataTypes/TopLevelBlock';
 import {cardBlockFactory} from 'CardBlocks/cardBlockFactory';
 import {vuBlockFactory} from 'VuBlocks/vuBlockFactory';
-
+import DataReader from "../utils/DataReader";
 
 export function blockFactory(data, pos): TopLevelBlock {
-    if (data[pos] == 0x76) {
+    if (DataReader.readUint8(data, pos) == 0x76) {
         return vuBlockFactory(data, pos);
     } else {
         return cardBlockFactory(data, pos);
